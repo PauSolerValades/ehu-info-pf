2a875bdc0414cd0e756e2f64f4852a2bf5851c0e
Pau <pau.soler.valades@gmail.com>
2020-11-11 02:56:16 +0100

    Fet la merda de arbres

--- Types/ExpArit.hs
+++ Types/ExpArit.hs
@@ -9,13 +9,13 @@ data Expr a =  N a
 
 
 
-ejem1 = N 5 :+: N 3 :+: N 4
+ejem1 = N 5.0 :+: N 3.0 :+: N 4.0
+ejem2 :: Expr Integer
 ejem2 = N 5 :+: N 8 :*: N 2
 -- por las expresiones: N 5 :+: (N8 :*: N 2) = 21
 -- NO (N 5 :+: N 8) :*: N 2 que seria 26
 
---evaluar :: Fractional a => Expr a -> a
---evaluar :: Fractional a => Expr a -> a
+evaluar :: Fractional a => Expr a -> a
 evaluar (N x) = x
 evaluar (e1 :+: e2) = evaluar e1 + evaluar e2
 evaluar (e1 :-: e2) = evaluar e1 - evaluar e2
@@ -26,34 +26,24 @@ evaluar (e1 :/: e2) = evaluar e1 / evaluar e2
 -- 1. Hacer (Exp a) instancia de la clase Show (mostrar como queramos) DONE
 -- 2. Hacer (Exp a) instancia de la clase Eq usando evaluar (tendremos que quitar el deriving)
 -- 3. Hacer (Exp a) instancia de la clase Num
--- Per fer això necessitaré entendre tota aquesta merda, nais. Em poso a la biblio de la resi i a tope
 
 instance Show a => Show (Expr a) where
-    show (N a) = show a
-    show (N a :+: N b) = "(" ++ show (N a) ++ "+" ++ show (N b) ++ ")"
-    show (N a :-: N b) = "(" ++ show (N a) ++ "-" ++ show (N b) ++ ")"
-    show (N a :*: N b) = "(" ++ show (N a) ++ "*" ++ show (N b) ++ ")"
-    show (N a :/: N b) = "(" ++ show (N a) ++ "/" ++ show (N b) ++ ")"
-    show (a :+: N c) = show(a) ++ "+" ++ show (N c)
-    show (a :-: N c) = show(a) ++ "-" ++ show (N c)
-    show (a :*: N c) = show(a) ++ "*" ++ show (N c)
-    show (a :/: N c) = show(a) ++ "/" ++ show (N c)
-    show (N a :+: b) = show(N a) ++ "+" ++ show b
-    show (N a :-: b) = show(N a) ++ "-" ++ show b
-    show (N a :*: b) = show(N a) ++ "*" ++ show b
-    show (N a :/: b) = show(N a) ++ "/" ++ show b
-
-{-
-Preguntar perque instance Show Expr where
-    show a = case a of ........ li falten coses / no funciona
--}
+    show a = case a of
+        (N a)     -> show a
+        (a :+: b) -> "(" ++ show a ++ "+" ++ show b ++ ")"
+        (a :-: b) -> "(" ++ show a ++ "-" ++ show b ++ ")"
+        (a :*: b) -> "(" ++ show a ++ "*" ++ show b ++ ")"
+        (a :/: b) -> "(" ++ show a ++ "/" ++ show b ++ ")" 
 
 instance (Eq a, Fractional a) => Eq (Expr a) where
     (N a) == (N b) = a==b
     a == b = evaluar a == evaluar b
 
---No et mentié, això no m'acaba de sortir...
+instance (Ord a, Fractional a) => Ord (Expr a) where
+    N a <= N b = a <= b
+    a <= b = evaluar a <= evaluar b
 
+{-
 instance Num a => Num (Expr a) where
     N a + N b = N (a+b)
     N a - N b = N (a-b)
@@ -61,5 +51,5 @@ instance Num a => Num (Expr a) where
     negate (N a) = N (-a)
     abs (N a) = N a
     signum (N a) = N a
-    fromInteger (a) = (N a)
-
+    fromInteger n = N n
+-}
--- Types/arboles.hs
+++ Types/arboles.hs
@@ -76,6 +76,7 @@ numVerif p = foldArBin f 0
     -- | p r = 1 + nvi + nvd
     -- | otherwise = nvi + nvd
 
+
 numVerif' p = foldArBin (\r vi vd -> (fromEnum (p r)) + vi + vd) 0
 
 -- EJERCICIO PARA CASA
@@ -85,21 +86,38 @@ type ArPares a = ArBin (a,a) --No es un tipo de datos algebraico nuevo, sinó qu
 -- y empleando la función del ejercicio numVerif determine 
 -- el número de nodos (x,y) tales que x es menor que y.
 
+ap1 = Nodo (Nodo (Nodo Vac (2,3) Vac)
+                (3,3) 
+                (Nodo Vac (6,5) Vac)) 
+          (6,0) 
+          (Nodo (Nodo Vac (7,6) Vac) 
+                (8,6) 
+                (Nodo Vac (5,9) Vac))              
+
+-- llamas a numVerif (\(x,y)->x>y) apn
 
 -- Supongamos el siguiente tipo de árboles binarios con información  
 -- de dos tipos en nodos internos y hojas: 
 
-data Arbol a b  =  Hoja a | NodoT  (Arbol a b) b (Arbol a b) deriving Show
+data Arbol a b  =  Hoja a | NodoT  (Arbol a b) b (Arbol a b)
 
 -- definimos el tipo de las expresiones aritm�ticas 
 -- en términos de este tipo de �rboles
 
 type ExpArit = Arbol Int String --tipo sinónimo de Arbol a b, dopnde a es int i string es b
 
+instance (Show a, Show b) => Show (Arbol a b) where
+    show a = case a of
+        (Hoja a) -> show a
+        (NodoT ard e ari) -> "(" ++ show ard ++ show e ++ show ari ++ ")"
+-- PREGUNTAR: COM FER UN INSTANCE SHOW DEL TYPE HEREDANT DEL GRAN.
 
 -- Ejemplos 
 exp1 :: ExpArit
 exp1 = Hoja 9
+
+--PREGUNAR: PERQUE QUAN CRIDO A EXP2 EM DIU QUE NO SAP ON ÉS LA VARIABLE
+exp2 :: Arbol Int String
 exp2 = NodoT (Hoja 3) "*" (Hoja 5)
 exp3 = NodoT exp1 "-" (NodoT (Hoja 10) "+" (Hoja 6))
 exp4 = NodoT exp3 "+" exp2
@@ -108,13 +126,24 @@ exp4 = NodoT exp3 "+" exp2
 -- Definir una funci�n
 
 foldArbol :: (t1 -> t2) -> (t3 -> t2 -> t2 -> t2) -> Arbol t1 t3 -> t2
-foldArbol f g (Hoja a) = f a
+foldArbol f _ (Hoja a) = f a
 foldArbol f g (NodoT ai r ad) = g r (foldArbol f g ai) (foldArbol f g ad)
    
--- Definir, en funci�n de foldArbol, funciones que permitan:
--- (a) calcular el n�mero de operadores de una expresi�n (es lo mismo que el número de nodos internos.)
-   -- f = (const 0) = (\x->0)
-   -- g r noi nod = 1 + noi + nod
+-- Definir, en función de foldArbol, funciones que permitan:
+-- (a) calcular el número de operadores de una expresi�n (es lo mismo que el número de nodos internos.)
 
 numOperadores = foldArbol (\x->0) (\_ opi opd -> opi + opd +1)
 -- (b) evaluar una expresi�n
+evaluar :: Arbol Int [Char] -> Int
+evaluar = foldArbol (\x->x) g
+    where
+        g op a b
+            | op == "+" = a + b
+            | op == "-" = a - b
+            | op == "*" = a * b
+
+--PREGUNTAR: PERQUE EM GENERA PROBLEMES D'INT A INTEGER, CANVIO PER INTEGER TOT I JA FUNCIONA QUE FLIPES
+--Autoresposta: Quan no determines el tipus d'algunes funcions a haskell, ell mateix assumeix el tipus que li agrada.
+-- En aquest case evaluar no tenia el typing, per tant assumia que volies el tipus Integer, pero quan li declares Int tot ja funcional
+
+--PREGUNTAR RESPECTE A LA RESPOSTA: Perque numOperadores funciona tot i tenint aquest problema sense declarar res però evaluar m'envia a la merda?
